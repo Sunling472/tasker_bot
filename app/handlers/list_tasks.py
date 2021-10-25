fc80560d56f70f63519d7daf6d11c1bbf9829d84
Sunling472 <wowlotten@yandex.ru>
2021-10-25 22:58:22 +0300

    inline_button

--- app/handlers/list_tasks.py
+++ app/handlers/list_tasks.py
@@ -3,17 +3,32 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text, IDFilter
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-from ..base import get_list_tasks
+from ..base import get_list_tasks, del_task
+
+
+def get_keyboard():
+    buttons: list = [
+        types.InlineKeyboardButton(text='Редактировать', callback_data='edit'),
+        types.InlineKeyboardButton(text='Удалить', callback_data='del')
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttons)
+    return keyboard
 
 
 async def list_tasks(message: types.Message):
+    keyboard = get_keyboard()
     user_id: int = message.from_user.id
     lt: list = get_list_tasks(user_id)
     if len(lt) == 0:
         await message.answer('Ни одного таска ещё не создано.')
     else:
         for task in lt:
-            await message.answer(f'{task[0]}\n\n{task[1]}\n\nid: {task[-1]}')
+            await message.answer(f'{task[0]}\n\n{task[1]}\n\nid: {task[-1]}', reply_markup=keyboard)
+
+
+async def inline_del_handler(call: types.CallbackQuery, message: types.Message):
+    pass
 
 
 def register_handlers_list_task(dp: Dispatcher, admin_id: int):
